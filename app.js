19202043b21001d68795c3ac13bf0ddbd2da25f0
Sameer Ahmed <balochtradersnayapay@gmail.com>
2024-11-09 19:53:21 +0500

    modified the operaters using condition

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 var display = document.getElementById("display");
+var Plus = document.getElementById("plus");
+var Minus = document.getElementById("minus");
+var Multiply = document.getElementById("multiply");
+var Divide = document.getElementById("divide");
 
+function plus(param) {
+  if (display.value.at(-1) == "+") {
+    Plus.Disable;
+  } else {
+    display.value += param;
+  }
+}
+function minus(param) {
+  if (display.value.at(-1) == "-") {
+    Minus.Disable;
+  } else {
+    display.value += param;
+  }
+}
+function multiply(param) {
+  if (display.value.at(-1) == "*") {
+    Multiply.Disable;
+  } else {
+    display.value += param;
+  }
+}
+function divide(param) {
+  if (display.value.at(-1) == "/") {
+    Divide.Disable;
+  } else {
+    display.value += param;
+  }
+}
 function numb(param) {
   if (display.value.at(-1) == "*") {
     var numb = document.getElementById("number");
     numb.Disable;
   } else {
-    display.value += param;
+    display.value += number;
   }
 }
 
